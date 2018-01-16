c602670d9ca44495e22b8171c917f8adb6822cd0
UmaMaheshK <uma.kondapally@gmail.com>
2018-01-16 20:05:44 +0530

    Add Web API

---
+++ DapperDemoAPI/Controllers/CustomerController.cs
@@ -0,0 +1,66 @@
+﻿using DapperDemoAPI.DAL;
+using DapperDemoAPI.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace DapperDemoAPI.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        private CustomerRespository _ourCustomerRespository;
+        //https://www.jeremymorgan.com/blog/programming/how-to-dapper-web-api/
+
+        public CustomerController()
+        {
+            _ourCustomerRespository = new CustomerRespository();
+        }
+
+        // GET: api/Customer
+        [Route("Customers")]
+        [HttpGet]
+        public List<Customer> Get()
+        {
+            return _ourCustomerRespository.GetCustomers(10, "ASC");
+        }
+
+        // GET: /Customer/10/ASC
+        [Route("Customers/{amount}/{sort}")]
+        [HttpGet]
+        public List<Customer> Get(int amount, string sort)
+        {
+            return _ourCustomerRespository.GetCustomers(amount, sort);
+        }
+
+        // GET: /Customer/5
+        [Route("Customers/{id}")]
+        [HttpGet]
+        public Customer Get(int id)
+        {
+            return _ourCustomerRespository.GetSingleCustomer(id);
+        }
+
+        // POST: api/Customer
+        [Route("Customers")]
+        [HttpPost]
+        public bool Post([FromBody]Customer ourCustomer)
+        {
+            return _ourCustomerRespository.InsertCustomer(ourCustomer);
+        }
+
+        // PUT: api/Customer/5
+        [Route("Customers")]
+        [HttpPut]
+        public bool Put([FromBody]Customer ourCustomer)
+        {
+            return _ourCustomerRespository.UpdateCustomer(ourCustomer);
+        }
+
+        // DELETE: api/Customer/5
+        [Route("Customers/{id}")]
+        [HttpDelete]
+        public bool Delete(int id)
+        {
+            return _ourCustomerRespository.DeleteCustomer(id);
+        }
+    }
+}
---
+++ DapperDemoAPI/DAL/CustomerRespository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DapperDemoAPI.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Dapper;
+
+namespace DapperDemoAPI.DAL
+{
+    //https://www.jeremymorgan.com/blog/programming/how-to-dapper-web-api/
+    public class CustomerRespository : ICustomerRespository
+    {
+        private IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+        public bool DeleteCustomer(int customerId)
+        {
+            int rowsAffected = this.db.Execute(@"DELETE FROM [jeremy].[Customer] WHERE CustomerID = @CustomerID", new { CustomerID = customerId });
+
+            if (rowsAffected > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public List<Customer> GetCustomers(int amount, string sort)
+        {
+            return this.db.Query<Customer>("SELECT TOP " + amount + " [CustomerID],[CustomerFirstName],[CustomerLastName],[IsActive] FROM [Customer] ORDER BY CustomerID " + sort).ToList();
+        }
+
+        public Customer GetSingleCustomer(int customerId)
+        {
+            return db.Query<Customer>("SELECT[CustomerID],[CustomerFirstName],[CustomerLastName],[IsActive] FROM [Customer] WHERE CustomerID =@CustomerID", new { CustomerID = customerId }).SingleOrDefault();
+        }
+
+        public bool InsertCustomer(Customer ourCustomer)
+        {
+            int rowsAffected = this.db.Execute(@"INSERT Customer([CustomerFirstName],[CustomerLastName],[IsActive]) values (@CustomerFirstName, @CustomerLastName, @IsActive)", new { CustomerFirstName = ourCustomer.CustomerFirstName, CustomerLastName = ourCustomer.CustomerLastName, IsActive = true });
+
+            if (rowsAffected > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool UpdateCustomer(Customer ourCustomer)
+        {
+            int rowsAffected = this.db.Execute("UPDATE [Customer] SET [CustomerFirstName] = @CustomerFirstName ,[CustomerLastName] = @CustomerLastName, [IsActive] = @IsActive WHERE CustomerID = " + ourCustomer.CustomerID, ourCustomer);
+
+            if (rowsAffected > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
